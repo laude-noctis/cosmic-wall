0a59c4d7a179c6dcb00dd96612f7235825eb0d9c
unknown <lizzybumpers11@gmail.com>
2023-11-15 06:08:58 -0600

    wrote questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,48 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Description",
+        message: "Write a Description",
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Write Installation instructions",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Write Usage instructions",
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "License:",
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Write the Contributors",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "How to run Tests",
+    },
+    {
+        type: "input",
+        name: "Questions",
+        message: "Contact information for Questions",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
